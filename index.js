1e341a2d578b4fe4412552fd9646c3f98aab4fbb
unknown <tom1k>
2020-07-07 12:27:51 +0100

    Finished Initial Cruz Response with Bot

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client();
-import token_file from 'token.js'
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const token = token_file;
-alert(token)
+const token = 'NzMwMDA2NjMxMDkzMzcwOTYy.XwRaZQ.AZfeMStkn5OUcwvQAM7mfwQj4TI';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.includes('cruz')) {
+    msg.reply('Ele não pode, está a jogar FiveM!');
+  }
+});
+
+client.on('message', msg => {
+    if (msg.content.includes('tumtumtum')) {
+      msg.reply('Oh, tou mesmo a ver que vai ser a mesma merda, vocês nunca marcam nada');
+    }
+  });
+
+client.login(token);
