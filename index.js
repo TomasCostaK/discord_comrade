8cd96d9e4c34eefb7c76552e2a144f5025f329ac
tom1k <tomascosta@ua.pt>
2020-07-11 23:13:32 +0100

    Started refactoring TOKEN to File

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+fs = require('fs');
 
 const token = 'NzMwMDA2NjMxMDkzMzcwOTYy.XwRaZQ.AZfeMStkn5OUcwvQAM7mfwQj4TI';
+//replace token with hidden file
+function loadInitialToken(){
+  fs.readFile('steamdb/steamdb.csv', function (err, fileData) {
+    console.log(fileData)
+  }
+}
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+//This function will be called at the start, so we load to an array all the free games available and then make a random choice
+const arrayFreeGames = []
+function loadFreeGames() {
+  var parse = require('csv-parse')
+  fs.readFile('steamdb/steamdb.csv', function (err, fileData) {
+    parse(fileData, {columns: false, trim: true}, function(err, rows) {
+      // Your CSV data is in an array of arrys passed to this callback as rows.
+      for (let index = 1; index < rows.length; index++) {
+        const element = rows[index];
+        if(element[1] == 'Free to Play'){
+          arrayFreeGames.push(element[0])
+        }
+      }
+    })
+  })
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  loadInitialToken()
+  loadFreeGames()
 });
 
 client.on('message', msg => {
-
     //Cruz initial meme
-  if (msg.content.includes('cruz')) {
-    msg.reply('Ele não pode, está a jogar FiveM!');
+  if (msg.content.includes('--random')) {
+    let rep = 'https://prnt.sc/p'+makeid(5);
+    msg.reply(rep);
   }
 
-  //TumTumTum
-  if (msg.content.includes('tumtumtum')) {
-    msg.reply('Oh, tou mesmo a ver que vai ser a mesma merda, vocês nunca marcam nada');
+  //Choose a free game from the ones we've loaded at node start
+  if (msg.content.includes('--free')) {
+    var item = arrayFreeGames[Math.floor(Math.random() * arrayFreeGames.length)];
+
+    let rep = 'https://store.steampowered.com/app/'+item;
+    msg.reply(rep);
   }
 
-  //Se a resposta vier do chico ou indiano
-  if(msg.author.tag === 'AceDestiny#3990') {
-    msg.reply('Que chico, vai brincar..');
-  }
-  else if (msg.author.tag === 'AceDestiny#3990') {
-    msg.reply('Cala-te, gualzito');
-  }
-
-  else if (msg.author.tag === 'joaocarvalho#9773') {
-    msg.reply('Tá calado Indiano');
+  if (msg.content.toLowerCase().includes('maria rodrigues')) {
+    msg.reply('https://prnt.sc/tdlp5v');
   }
 
 });
